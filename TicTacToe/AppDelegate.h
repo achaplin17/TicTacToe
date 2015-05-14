a88c6b3e693c05a3f90ad58a5f9f427b57c29c18
Anders Chaplin <achaplin17@cmc.edu>
2015-05-14 14:29:53 -0700

    Initial Commit

---
+++ TicTacToe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicTacToe
+//
+//  Created by Anders Chaplin on 5/14/15.
+//  Copyright (c) 2015 ___AndersChaplin___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TicTacToe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TicTacToe
+//
+//  Created by Anders Chaplin on 5/14/15.
+//  Copyright (c) 2015 ___AndersChaplin___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
