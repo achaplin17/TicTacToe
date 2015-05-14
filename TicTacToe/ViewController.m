50b8749e3c870c1a33bd41b5eb6574b245883cc9
Anders Chaplin <achaplin17@cmc.edu>
2015-05-14 16:16:45 -0700

    Functional game w/o result

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -9,6 +9,21 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *label1;
+@property (weak, nonatomic) IBOutlet UILabel *label2;
+@property (weak, nonatomic) IBOutlet UILabel *label3;
+@property (weak, nonatomic) IBOutlet UILabel *label4;
+@property (weak, nonatomic) IBOutlet UILabel *label5;
+@property (weak, nonatomic) IBOutlet UILabel *label6;
+@property (weak, nonatomic) IBOutlet UILabel *label7;
+@property (weak, nonatomic) IBOutlet UILabel *label8;
+@property (weak, nonatomic) IBOutlet UILabel *label9;
+@property (weak, nonatomic) IBOutlet UILabel *whichPlayerLabel;
+
+@property int turn;
+@property int currentPlayer;
+
+@property NSArray *labelsArray;
 
 @end
 
@@ -16,12 +31,43 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    self.labelsArray = [NSArray arrayWithObjects:self.label1, self.label2, self.label3, self.label4, self.label5, self.label6, self.label7, self.label8, self.label9, nil];
+    self.turn = 1;
+    self.currentPlayer = 1;
+    self.whichPlayerLabel.text = [NSString stringWithFormat:@"Now it's Player %d's turn", self.currentPlayer];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+
+- (IBAction)onLabelTapped:(UITapGestureRecognizer *)sender {
+    self.turn++;
+    CGPoint tappedPoint = [sender locationInView:self.view];
+    [self findLabelUsingPoint:tappedPoint];
+}
+
+
+
+- (void)findLabelUsingPoint:(CGPoint)point {
+    if(self.turn%2 > 0) {
+        self.currentPlayer = 1;
+    } else {
+        self.currentPlayer = 2;
+    }
+
+    self.whichPlayerLabel.text = [NSString stringWithFormat:@"Now it's Player %d's turn", self.currentPlayer];
+
+    for (UILabel *label in self.labelsArray) {
+        if(CGRectContainsPoint(label.frame, point)) {
+            if (self.currentPlayer == 1) {
+                label.textColor = [UIColor blueColor];
+                label.text = @"X";
+            } else {
+                label.textColor = [UIColor redColor];
+                label.text = @"O";
+            }
+        }
+    }
 }
 
 @end
